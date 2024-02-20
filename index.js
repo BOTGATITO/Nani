07b73d96514998a8b5b70ce83467f46cdad8608c
BOTGATITO <149286603+BOTGATITO@users.noreply.github.com>
2024-02-19 22:56:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const { name, author } = require(join(__dirname, "./package.json"));
 const { say } = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 
-say('Celosa\nBot', {
+say('Proyecto\nX', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
