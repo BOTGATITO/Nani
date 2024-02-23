16f542c069d704d660b44f23cb2c7c2afe8f1573
BOTGATITO <149286603+BOTGATITO@users.noreply.github.com>
2024-02-22 21:30:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const { name, author } = require(join(__dirname, "./package.json"));
 const { say } = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 
-say('ABADDON\nBOT', {
+say('RAGE\nSPORT', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
