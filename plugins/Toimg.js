e1e662970d7be9b9ef230489fe75b88d0a84e09b
BOTGATITO <149286603+BOTGATITO@users.noreply.github.com>
2024-04-12 23:52:23 -0500

    Update Toimg.js

--- plugins/Toimg.js
+++ plugins/Toimg.js
@@ -1,21 +1,23 @@
-import { webp2png } from '../lib/webp2mp4.js'
+import {webp2png} from '../lib/webp2mp4.js';
 
-var handler = async (m, { conn, usedPrefix, command }) => {
 
-const notStickerMessage = `*⚠️ RESPONDA CON ${usedPrefix + command} AL STICKER QUE DESEE CONVERTIR EN IMAGEN*`
-if (!m.quoted) throw notStickerMessage 
-const q = m.quoted || m
-let mime = q.mediaType || ''
-if (!/sticker/.test(mime)) throw notStickerMessage
-let media = await q.download()
-let out = await webp2png(media).catch(_ => null) || Buffer.alloc(0)
-await conn.sendFile(m.chat, out, 'error.png', null, fkontak, m)
+const handler = async (m, {conn, usedPrefix, command}) => {
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
+  const tradutor = _translate.plugins.convertidor_toimg
 
-}
-handler.help = ['toimg']
-handler.tags = ['transformador']
-handler.command = ['toimg', 'jpg', 'jpge', 'png']
 
-handler.limit = true
-
-export default handler
+  const notStickerMessage = `*${tradutor.texto1} ${usedPrefix + command}*`;
+  if (!m.quoted) throw notStickerMessage;
+  const q = m.quoted || m;
+  const mime = q.mediaType || '';
+  if (!/sticker/.test(mime)) throw notStickerMessage;
+  const media = await q.download();
+  const out = await webp2png(media).catch((_) => null) || Buffer.alloc(0);
+  await conn.sendFile(m.chat, out, 'error.png', null, m);
+};
+handler.help = ['toimg (reply)'];
+handler.tags = ['sticker'];
+handler.command = ['toimg', 'jpg', 'img'];
+export default handler;
