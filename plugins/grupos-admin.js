45e0e42144441b4a375d367bef18d8e14444da77
BOTGATITO <149286603+BOTGATITO@users.noreply.github.com>
2024-04-15 16:48:40 -0500

    Update grupos-admin.js

--- plugins/grupos-admin.js
+++ plugins/grupos-admin.js
@@ -167,11 +167,11 @@ global.dfail('admin', m, conn)
 return false
 }
 let pesan = args.join` `
-let oi = `» 🌺${lenguajeGB['smsAddB5']()} ${pesan}`
+let oi = `» 🌀 ${lenguajeGB['smsAddB5']()} ${pesan}`
 let teks = `${lenguajeGB['smstagaa']()}\n\n${oi}\n\n`
 for (let mem of participants) {
-teks += `➥🌷@${mem.id.split('@')[0]}\n`}
-teks += `𝙋𝙍𝙊𝙔𝙀𝘾𝙏𝙊 𝙓`
+teks += `➥🐾@${mem.id.split('@')[0]}\n`}
+teks += `*BELLOTA BOT²*`
 await conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )    
 break
     
